9664850e0ac1d2f8f9c9d0ef2d63bd0b0e3fb823
hytcwentao <453167555@qq.com>
2014-09-01 16:59:36 +0800

    Qzone1.4
    完成度40%

---
+++ Qzone1.4/js/index.js
@@ -0,0 +1,19 @@
+$(function(){
+
+	
+		$(".downli").hover(function(){
+			$(".slidedown").slideDown();
+			$(".downli").hide();
+		},function(){
+			
+		});
+			$(".downlis").hover(function(){
+			$(".slidedown2").slideDown();
+			$(".downlis").hide();
+		},function(){
+			
+		});
+	
+		
+	
+});
